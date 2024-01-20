eb221f2043b80dc046442a88229fb982015a9569
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2024-01-19 20:58:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-echo -e "Instalando dependências manual mente"
+echo -e "Instalando YAY"
 
-git clone https://aur.archlinux.org/yay.git;
+git clone https://aur.archlinux.org/yay.git
 
 echo "Nome de usuário"
-
 read user
 
-sudo chown -R $user:$user ./yay-git
-cd yay
+sudo chown -R $user:$user ./yay
 
+cd yay
 makepkg -si
 
 cd ~/
 
-echo "Baxando dependências "
+echo "Baixando dependências "
 sleep 4
 
 sudo yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus
 
-sudo pacman -S hyprland 
+sudo pacman -S hyprland
 
-
-#v0.0.2
+# V:0.0.3
