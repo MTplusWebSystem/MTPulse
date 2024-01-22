0e7cd96643f79a2ea15dc28a0ff892998577a9cc
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-01-22 00:02:24 -0400

    v:0.9

--- install.sh
+++ install.sh
@@ -20,5 +20,17 @@ sleep 4
 sudo yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus
 
 sudo pacman -S hyprland
+echo "Criando diretorios"
+sleep 4
+mkdir ~/Downloads
+mkdir ~/Musicas
+mkdir ~/Videos
+mkdir ~/Imagens
 
-# V:0.0.3
+mv hypr ~/.config/
+mv sdt ~/.config/
+mv extras ~/.config/
+mv waybar ~/.config/
+mv kitty ~/.config/
+
+# V:0.9
