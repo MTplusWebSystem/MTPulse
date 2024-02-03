f1e7cc14fbe5feee613b2c806b369f9f439f3856
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2024-02-03 13:45:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,35 +17,47 @@ cd ~/
 echo "Baixando dependências "
 sleep 4
 
-sudo yay -S gdb
-sudo yay -S ninja
-sudo yay -S gcc 
-sudo yay -S cmake 
-sudo yay -S meson 
-sudo yay -S libxcb 
-sudo yay -S xcb-proto 
-sudo yay -S xcb-util 
-sudo yay -S xcb-util-keysyms 
-sudo yay -S libxfixes 
-sudo yay -S libx11 
-sudo yay -S libxcomposite 
-sudo yay -S xorg-xinput 
-sudo yay -S libxrender 
-sudo yay -S pixman 
-sudo yay -S wayland-protocols 
-sudo yay -S cairo 
-sudo yay -S pango 
-sudo yay -S seatd 
-sudo yay -S libxkbcommon 
-sudo yay -S xcb-util-wm 
-sudo yay -S xorg-xwayland 
-sudo yay -S libinput 
-sudo yay -S libliftoff 
-sudo yay -S libdisplay-info 
-sudo yay -S cpio 
-sudo yay -S tomlplusplus
+yay -S --noconfirm gdb
+yay -S --noconfirm ninja
+yay -S --noconfirm gcc 
+yay -S --noconfirm cmake 
+yay -S --noconfirm meson 
+yay -S --noconfirm libxcb 
+yay -S --noconfirm xcb-proto 
+yay -S --noconfirm xcb-util 
+yay -S --noconfirm xcb-util-keysyms 
+yay -S --noconfirm libxfixes 
+yay -S --noconfirm libx11 
+yay -S --noconfirm libxcomposite 
+yay -S --noconfirm xorg-xinput 
+yay -S --noconfirm libxrender 
+yay -S --noconfirm pixman 
+yay -S --noconfirm wayland-protocols 
+yay -S --noconfirm cairo 
+yay -S --noconfirm pango 
+yay -S --noconfirm seatd 
+yay -S --noconfirm libxkbcommon 
+yay -S --noconfirm xcb-util-wm 
+yay -S --noconfirm xorg-xwayland 
+yay -S --noconfirm libinput 
+yay -S --noconfirm libliftoff 
+yay -S --noconfirm libdisplay-info 
+yay -S --noconfirm cpio 
+yay -S --noconfirm tomlplusplus
+yay -S --noconfirm hyprland
+yay -S --noconfirm kitty 
+yay -S --noconfirm waybar 
+yay -S --noconfirm swww 
+yay -S --noconfirm swaylock-effects 
+yay -S --noconfirm wofi 
+yay -S --noconfirm wlogout 
+yay -S --noconfirm mako 
+yay -S --noconfirm xdg-desktop-portal-hyprland 
+yay -S --noconfirm swappy 
+yay -S --noconfirm grim 
+yay -S --noconfirm slurp 
+yay -S --noconfirm thunar
 
-sudo pacman -S hyprland
 echo "Criando diretorios"
 sleep 4
 mkdir ~/Downloads
@@ -54,4 +66,4 @@ mkdir ~/Videos
 mkdir ~/Imagens
 
 
-# V:0.9
+# V:0.1.0
